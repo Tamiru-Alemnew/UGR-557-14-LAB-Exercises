0027264806c4a2ccba37a07ecdda9cc0c67ad0bd
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-04-08 16:21:28 +0300

    added excercise two  of navigation 2.0

---
+++ lab-3 Navigators/navigator 2.0/Exercise_2.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CourseApp());
+}
+
+class Course {
+  String code;
+  String title;
+  String description;
+  Course({required this.code, required this.title, this.description = ""});
+}
+
+class CoursesListScreen extends StatelessWidget {
+  final List<Course> courses;
+  final ValueChanged<Course> onTapped;
+
+  const CoursesListScreen({required this.courses, required this.onTapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Courses List")),
+      body: ListView(
+        children: [
+          for (var course in courses)
+            ListTile(
+              title: Text(course.title),
+              subtitle: Text(course.code),
+              onTap: () => onTapped(course),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class CourseApp extends StatefulWidget {
+  const CourseApp({Key? key}) : super(key: key);
+  @override
+  State<CourseApp> createState() => _CourseAppState();
+}
+
+class _CourseAppState extends State<CourseApp> {
+  Course? _selectedCourse;
+
+  List<Course> courses = [
+    Course(
+        code: "SiTE-001",
+        title: "Introduction To Programming",
+        description: "Computer Organization, Architecture, Programming"),
+  ];
+
+  void _tabHandler(Course course) {
+    setState(() {
+      _selectedCourse = course;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Navigator(
+        onPopPage: (route, result) {
+          if (!route.didPop(result)) {
+            return false;
+          }
+          setState(() {
+            _selectedCourse = null;
+          });
+          return true;
+        },
+        pages: [
+          MaterialPage(
+            key: const ValueKey('CourseListScreen'),
+            child: CoursesListScreen(
+              courses: courses,
+              onTapped: _tabHandler,
+            ),
+          ),
+          if (_selectedCourse != null)
+            MaterialPage(
+                key: ValueKey(_selectedCourse),
+                child: CourseDetailsScreen(
+                  course: _selectedCourse!,
+                ))
+        ],
+      ),
+    );
+  }
+}
+
+class CourseDetailsScreen extends StatelessWidget {
+  final Course course;
+
+  const CourseDetailsScreen({required this.course});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(course.title)),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(course.description),
+      ),
+    );
+  }
+}
