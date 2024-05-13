47de624b44cad96ae8a61175df19700edf6b504e
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-13 15:12:55 +0300

    added exercise two  loops  of kotline exercise

---
+++ Kotline-exercises/loops/exercise._two.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val passwordLength = 8 // Specify the length of the password
+    val password = generateRandomPassword(passwordLength)
+    println("Generated Password: $password")
+}
+
+fun generateRandomPassword(length: Int): String {
+    val allowedChars = ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf('!', '@', '#', '$', '%', '^', '&', '*')
+    val password = StringBuilder(length)
+
+    repeat(length) {
+        val randomIndex = (0 until allowedChars.size).random()
+        password.append(allowedChars[randomIndex])
+    }
+
+    return password.toString()
+}
