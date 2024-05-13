eff93dd1ebaa45771ad831996b7bf3a8842bd787
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-05-13 14:40:47 +0300

    added exercise two  of kotline exercise

---
+++ Kotline-exercises/variables/exercise_two.kt
@@ -0,0 +1,9 @@
+fun main() {
+    
+    val distance = 100.0 // distance value
+    val initialUnit = "kilometers" // initial unit
+
+    val convertedDistance = distance * 0.621371 // 1 kilometer = 0.621371 miles
+
+    println("Converted distance: $convertedDistance miles")
+}
